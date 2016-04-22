588349a0022d125269c033bbf836bcc41bf003fa
Fernando Cancellara <fdcancellara@gmail.com>
2016-04-22 16:53:28 -0300

    First instance of GameDoor.

---
+++ engine/src/main/java/ar/fiuba/tdd/tp/motor/games/opendoor/GameOpenDoor.java
@@ -0,0 +1,37 @@
+package ar.fiuba.tdd.tp.motor.games.opendoor;
+
+import ar.fiuba.tdd.tp.motor.Game;
+
+public class GameOpenDoor implements Game {
+
+    private boolean doorOpen;
+    private boolean playerHasKey;
+
+    public GameOpenDoor() {
+        this.doorOpen = false;
+        this.playerHasKey = false;
+    }
+
+    public void pickKey() {
+        this.playerHasKey = true;
+    }
+
+    public void openDoor() {
+        if (playerHasKey) {
+            this.doorOpen = true;
+        }
+    }
+
+    public boolean checkIfGameIsFinished() {
+        //Game is won if door is opened
+        return this.doorOpen;
+    }
+
+    public boolean getPlayerHasKey() {
+        return this.playerHasKey;
+    }
+
+    public boolean getDoorOpen() {
+        return this.doorOpen;
+    }
+}
