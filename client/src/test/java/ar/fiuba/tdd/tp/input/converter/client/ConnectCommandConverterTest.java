82829f9335979eedd15c5ab13e8536ba448bb1fe
German Guzelj <german.guzelj@almundo.com>
2016-04-23 19:58:06 -0300

    Adding ConnectComand and HelpCommand test

---
+++ client/src/test/java/ar/fiuba/tdd/tp/input/converter/client/ConnectCommandConverterTest.java
@@ -0,0 +1,13 @@
+package ar.fiuba.tdd.tp.input.converter.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConnectCommandConverterTest {
+
+    @Test
+    public void testDoConvert() throws Exception {
+
+    }
+}
---
+++ client/src/test/java/ar/fiuba/tdd/tp/input/converter/client/HelpCommandConverterTest.java
@@ -0,0 +1,19 @@
+package ar.fiuba.tdd.tp.input.converter.client;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelpCommandConverterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDoConvert() throws Exception {
+
+    }
+}
