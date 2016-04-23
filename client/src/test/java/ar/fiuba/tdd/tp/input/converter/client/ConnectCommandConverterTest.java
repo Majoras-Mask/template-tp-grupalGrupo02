873d99b3397265e8e62999fbfad4e51924c41eea
German Guzelj <german.guzelj@almundo.com>
2016-04-23 20:06:49 -0300

    Ading test

--- client/src/test/java/ar/fiuba/tdd/tp/input/converter/client/ConnectCommandConverterTest.java
+++ client/src/test/java/ar/fiuba/tdd/tp/input/converter/client/ConnectCommandConverterTest.java
@@ -1,13 +1,54 @@
 package ar.fiuba.tdd.tp.input.converter.client;
 
+import ar.fiuba.tdd.tp.ClientV2;
+import ar.fiuba.tdd.tp.config.ConnectorSettings;
+import ar.fiuba.tdd.tp.input.command.client.ConnectCommand;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class ConnectCommandConverterTest {
 
+    private ClientV2 client;
+    private ConnectCommandConverter connectCommandConverter;
+
+    @Before
+    public void setUp() {
+        this.client = mock(ClientV2.class);
+        this.connectCommandConverter = new ConnectCommandConverter(this.client);
+    }
+
     @Test
     public void testDoConvert() throws Exception {
+        final ConnectCommand command1 = this.connectCommandConverter.doConvert("connect 127.0.0.1:8080");
+        assertEquals(command1, newExpected(this.client, "127.0.0.1", 8080));
 
+        final ConnectCommand command2 = this.connectCommandConverter.doConvert("connect 127.0.0.1:16573");
+        assertEquals(command2, newExpected(this.client, "127.0.0.1", 16573));
+
+        final ConnectCommand command3 = this.connectCommandConverter.doConvert("connect 192.23.1.3:40394");
+        assertEquals(command3, newExpected(this.client, "192.23.1.3", 40394));
     }
+
+    @Test(expected = IllegalStateException.class)
+    public void conversion_should_fail() throws Exception {
+        this.connectCommandConverter.convert("test");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void conversion_should_fail_on_wrong_host() throws Exception {
+        this.connectCommandConverter.convert("connect 192.23.1:40394");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void conversion_should_fail_on_wrong_port() throws Exception {
+        this.connectCommandConverter.convert("connect 192.23.1.3:403942");
+    }
+
+    private ConnectCommand newExpected(ClientV2 client, String host, int port) {
+        return new ConnectCommand(client, new ConnectorSettings(host, port));
+    }
+
 }
--- client/src/test/java/ar/fiuba/tdd/tp/input/converter/client/HelpCommandConverterTest.java
+++ client/src/test/java/ar/fiuba/tdd/tp/input/converter/client/HelpCommandConverterTest.java
@@ -1,19 +1,45 @@
 package ar.fiuba.tdd.tp.input.converter.client;
 
+import ar.fiuba.tdd.tp.ClientV2;
+import ar.fiuba.tdd.tp.input.command.client.HelpCommand;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class HelpCommandConverterTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private HelpCommandConverter helpCommandConverter;
+    private ClientV2 client;
 
+    @Before
+    public void setUp() {
+        this.client = mock(ClientV2.class);
+        this.helpCommandConverter = new HelpCommandConverter(this.client);
     }
 
     @Test
     public void testDoConvert() throws Exception {
+        final HelpCommand command1 = this.helpCommandConverter.doConvert("help game1");
+        assertEquals(command1, newExpected(this.client, "game1"));
 
+        final HelpCommand command2 = this.helpCommandConverter.doConvert("help help");
+        assertEquals(command2, newExpected(this.client, "help"));
     }
+
+    @Test(expected = IllegalStateException.class)
+    public void conversion_should_fail() throws Exception {
+        this.helpCommandConverter.convert("test");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void conversion_should_fail_on_wrong_host() throws Exception {
+        this.helpCommandConverter.convert("hepl game");
+    }
+
+    private HelpCommand newExpected(ClientV2 client, String game1) {
+        return new HelpCommand(client, game1);
+    }
+
 }
