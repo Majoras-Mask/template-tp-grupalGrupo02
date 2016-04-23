6bf7b1d5aa1e77b5c9db918adc4b2a079d970944
German Guzelj <german.guzelj@almundo.com>
2016-04-23 20:25:14 -0300

    Removing old classes

--- client/src/main/java/ar/fiuba/tdd/tp/Main.java
+++ client/src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -1,20 +1,18 @@
 package ar.fiuba.tdd.tp;
 
-import java.io.UnsupportedEncodingException;
-
 public class Main {
     public static void main(String[] args) {
 
-        Client client = null;
-        try {
-            client = new Client();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        boolean successfulConnection = client.runDisconnected();
-        if (successfulConnection) {
-            client.runConnected();
-        }
+//        Client client = null;
+//        try {
+//            client = new Client();
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//            System.exit(1);
+//        }
+//        boolean successfulConnection = client.runDisconnected();
+//        if (successfulConnection) {
+//            client.runConnected();
+//        }
     }
 }
