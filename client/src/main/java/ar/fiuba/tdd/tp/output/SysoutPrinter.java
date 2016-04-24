13be4971e0189c9297d17321b4a7718f6985a79a
German Guzelj <german.guzelj@almundo.com>
2016-04-23 21:28:33 -0300

    SysoutPrinter refactor

--- client/src/main/java/ar/fiuba/tdd/tp/output/SysoutPrinter.java
+++ client/src/main/java/ar/fiuba/tdd/tp/output/SysoutPrinter.java
@@ -1,15 +1,29 @@
 package ar.fiuba.tdd.tp.output;
 
 import ar.fiuba.tdd.tp.output.consumer.ClientConsumer;
-import ar.fiuba.tdd.tp.output.consumer.ClientEvent;
+import ar.fiuba.tdd.tp.output.consumer.ClientResponse;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class SysoutPrinter implements ClientConsumer {
 
     private static final String PREFIX = "Client> ";
 
+    private final BufferedWriter outputBuffer;
+
+    public SysoutPrinter(BufferedWriter outputBuffer) {
+        this.outputBuffer = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
+    }
+
     @Override
-    public void consume(ClientEvent event) {
-        //TODO mejorar esto...
-        System.out.println(PREFIX + event.getEvent());
+    public void consume(ClientResponse response) {
+        try {
+            this.outputBuffer.write(PREFIX + response.getEvent());
+            this.outputBuffer.flush();
+        } catch (Exception e) {
+            throw new IllegalStateException("Unexpected error occurred while writing to output buffer: " + e);
+        }
     }
 }
