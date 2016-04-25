f20e7509aff34866f2c8c651b1487e9a1823df3b
Fernando Cancellara <fdcancellara@gmail.com>
2016-04-25 12:58:28 -0300

    Added the game open door two, and a mini test.

--- engine/src/main/java/ar/fiuba/tdd/tp/motor/game/games/zorktype/openDoorTwo/GameOpenDoorTwo.java
+++ engine/src/main/java/ar/fiuba/tdd/tp/motor/game/games/zorktype/openDoorTwo/GameOpenDoorTwo.java
@@ -1,20 +1,29 @@
 package ar.fiuba.tdd.tp.motor.game.games.zorktype.opendoortwo;
 
+import ar.fiuba.tdd.tp.motor.game.components.ComponentBox;
+import ar.fiuba.tdd.tp.motor.game.components.ComponentDoor;
+import ar.fiuba.tdd.tp.motor.game.components.ComponentKey;
 import ar.fiuba.tdd.tp.motor.game.components.ComponentRoom;
 import ar.fiuba.tdd.tp.motor.game.games.zorktype.ZorkTypeGame;
 
 public class GameOpenDoorTwo extends ZorkTypeGame {
 
-   // private ComponentRoom winningRoom;
+    private ComponentRoom winningRoom;
 
     public GameOpenDoorTwo() {
-        //ComponentRoom roomOne = new ComponentRoom();
-        //this.winningRoom = new ComponentRoom();
-        //ComponentBox
+        ComponentRoom roomOne = new ComponentRoom();
+        this.winningRoom = new ComponentRoom();
+        ComponentBox box = new ComponentBox();
+        ComponentKey key = new ComponentKey();
+        box.addComponent(key);
+        roomOne.addComponent(box);
+        ComponentDoor door = new ComponentDoor(this, this.winningRoom, key);
+        roomOne.addComponent(door);
+        this.currentRoom = roomOne;
     }
 
     @Override
     public boolean checkIfGameIsFinished() {
-        return true;
+        return (getCurrentRoom() == this.winningRoom);
     }
 }
