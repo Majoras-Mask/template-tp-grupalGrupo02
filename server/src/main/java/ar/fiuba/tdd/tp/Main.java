57cb4291cd06df5791d50bec35fd33f5fbf3f5c9
Nimble-Momonga <manuel_tejedor_77@hotmail.com>
2016-04-26 15:20:18 -0300

    Removed old commented code in Main.java

--- server/src/main/java/ar/fiuba/tdd/tp/Main.java
+++ server/src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -13,34 +13,5 @@ public class Main {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-
-        /*int portNumber = 8000;
-​
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-        System.out.println("port " + Integer.toString(portNumber) + " opened");
-        Socket clientSocket = serverSocket.accept();
-        BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8")), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
-​
-        String inputLine;
-        String outputLine;
-​
-        // Initiate conversation with client
-        outputLine = "talk to me my son";
-        out.println(outputLine);
-​
-        try {
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Client> " + inputLine);
-                outputLine = consoleInput.readLine();
-                out.println(outputLine);
-                if (outputLine == null || outputLine.equals("close")) {
-                    break;
-                }
-            }
-        } catch (SocketException e) {
-            System.out.println("Client disconnected");
-        }*/
     }
 }
