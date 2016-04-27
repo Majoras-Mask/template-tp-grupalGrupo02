66b9100cd521b5a68dbd6dacefed5673dcde9279
mtejedor <mtejedor@hexacta.com>
2016-04-27 01:14:05 -0300

    Revert "Refactor in Connection.java related to issue #8"
    This reverts commit 30ec2a3d3762be17820f59585692e31061f436dd.

--- server/src/main/java/ar/fiuba/tdd/tp/api/Connection.java
+++ server/src/main/java/ar/fiuba/tdd/tp/api/Connection.java
@@ -1,28 +1,30 @@
 package ar.fiuba.tdd.tp.api;
 
+import ar.fiuba.tdd.tp.api.Request;
+import ar.fiuba.tdd.tp.api.Response;
+
 import java.io.*;
+
 import java.net.ServerSocket;
 import java.net.Socket;
-import static java.util.Objects.isNull;
-import static java.util.Objects.requireNonNull;
+import java.net.SocketException;
 
 public class Connection extends Thread {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private ObjectOutputStream outputStream;
     private ObjectInputStream inputStream;
-    private Request request;
-    private Response response;
+
     //private Game game;
 
     public Connection(ServerSocket serverSocket/*, Game game*/) {
-        this.serverSocket = requireNonNull(serverSocket);
+        this.serverSocket = serverSocket;
         //this.game = game;
     }
 
     public void closeConnection() {
         try {
-            if (!isNull(clientSocket) && !clientSocket.isClosed()) {
+            if (clientSocket != null && !clientSocket.isClosed()) {
                 clientSocket.close();
             }
             serverSocket.close();
@@ -36,35 +38,32 @@ public class Connection extends Thread {
         this.inputStream = new ObjectInputStream(clientSocket.getInputStream());
     }
 
-    private void speak() throws IOException, ClassNotFoundException {
-        welcome();
-        boolean exit = false;
-        while (!exit && !isNull(request = (Request) inputStream.readObject())) {
-            if (request.getSomething().equals("exit game")) {
-                //TODO: send closure to client
-                exit = true;
-            }
-            //TODO: send client request to game
-            //TODO: send game response to client
-        }
-    }
-
-    private void welcome() throws IOException {
-        response = new Response("Welcome");
-        outputStream.writeObject(response);
-        outputStream.flush();
-    }
-
     public void run() {
+        Request inputLine;
         try {
-            while (!serverSocket.isClosed() && !isNull(clientSocket = serverSocket.accept())) {
-                //TODO: inform that new connection was established
+            while (!serverSocket.isClosed() && (clientSocket = serverSocket.accept()) != null) {
+                System.out.println("socket connected");
                 getStream(clientSocket);
-                speak();
+                Response res = new Response("Hello there?");
+                outputStream.writeObject(res);
+                outputStream.flush();
+
+                while ((inputLine = (Request) inputStream.readObject()) != null) {
+                    //intercept message
+                    //send message to game
+                    //send message to client
+                    System.out.println(inputLine.getSomething());
+                }
+                System.out.println("closing clientSocket");
                 clientSocket.close();
-                //TODO: inform that client was disconnected
             }
-        } catch (ClassNotFoundException | IOException e) {
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
