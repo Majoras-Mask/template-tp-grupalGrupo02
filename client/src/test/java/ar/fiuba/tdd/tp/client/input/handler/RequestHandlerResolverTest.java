124f540b66706f09157a17d57b5bc4b4fc32e8cb
German Guzelj <german.guzelj@almundo.com>
2016-04-27 17:48:59 -0300

    Fixing checkstyle

--- client/src/test/java/ar/fiuba/tdd/tp/client/input/handler/RequestHandlerResolverTest.java
+++ client/src/test/java/ar/fiuba/tdd/tp/client/input/handler/RequestHandlerResolverTest.java
@@ -1,19 +1,56 @@
 package ar.fiuba.tdd.tp.client.input.handler;
 
+import ar.fiuba.tdd.tp.client.exception.ClientException;
+import ar.fiuba.tdd.tp.client.input.ClientRequest;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestHandlerResolverTest {
 
+    private static final RequestHandler handler1 = Mockito.mock(RequestHandler.class);
+    private static final RequestHandler handler2 = Mockito.mock(RequestHandler.class);
+    private static final RequestHandler handler3 = Mockito.mock(RequestHandler.class);
+
+    private static final ClientRequest supported = new ClientRequest("supported");
+    private static final ClientRequest notSupported = new ClientRequest("notSupported");
+
+    private RequestHandlerResolver resolver;
+
     @Before
     public void setUp() throws Exception {
-
+        this.resolver = new RequestHandlerResolver(createHandlers());
     }
 
     @Test
-    public void testResolve() throws Exception {
+    public void testShouldResolveHandler() throws Exception {
+        Assert.assertEquals(this.resolver.resolve(supported), handler2);
+    }
 
+    @Test(expected = ClientException.class)
+    public void testShouldFailOnUnsupported() throws Exception {
+        Assert.assertEquals(this.resolver.resolve(notSupported), handler2);
+    }
+
+
+    private List<RequestHandler> createHandlers() {
+        Mockito.when(handler1.supports(supported)).thenReturn(Boolean.FALSE);
+        Mockito.when(handler2.supports(supported)).thenReturn(Boolean.TRUE);
+        Mockito.when(handler3.supports(supported)).thenReturn(Boolean.FALSE);
+
+        Mockito.when(handler1.supports(notSupported)).thenReturn(Boolean.FALSE);
+        Mockito.when(handler2.supports(notSupported)).thenReturn(Boolean.FALSE);
+        Mockito.when(handler3.supports(notSupported)).thenReturn(Boolean.FALSE);
+
+        return new ArrayList<RequestHandler>() { {
+                add(handler1);
+                add(handler2);
+                add(handler3);
+            }
+        };
     }
 }
