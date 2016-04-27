f87341c1b983b775944bdf1432e34881fef15a90
Fernando Cancellara <fdcancellara@gmail.com>
2016-04-27 10:55:35 -0300

    Added consume chain and command.

--- engine/src/main/java/ar/fiuba/tdd/tp/motor/commands/zorktype/ZorkCommandActionable.java
+++ engine/src/main/java/ar/fiuba/tdd/tp/motor/commands/zorktype/ZorkCommandActionable.java
@@ -1,7 +1,5 @@
 package ar.fiuba.tdd.tp.motor.commands.zorktype;
 
-
-import ar.fiuba.tdd.tp.motor.game.components.GameComponent;
 import ar.fiuba.tdd.tp.motor.game.games.zorktype.ZorkTypeGame;
 
 public abstract class ZorkCommandActionable extends ZorkCommand {
