262c9c3eca5c14d4e9302c1aed22beb171c0f4ba
German Guzelj <german.guzelj@almundo.com>
2016-04-27 17:47:12 -0300

    Adding RequestHandlerResolverTest

---
+++ client/src/test/java/ar/fiuba/tdd/tp/client/input/handler/RequestHandlerResolverTest.java
@@ -0,0 +1,19 @@
+package ar.fiuba.tdd.tp.client.input.handler;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestHandlerResolverTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testResolve() throws Exception {
+
+    }
+}
