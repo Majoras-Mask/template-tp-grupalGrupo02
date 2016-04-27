ef48e497e6dd28c56c5ad9265d8b96fd34b7598e
mtejedor <mtejedor@hexacta.com>
2016-04-27 01:09:04 -0300

    taking console management out of Connection.java

--- server/src/main/java/ar/fiuba/tdd/tp/api/Request.java
+++ server/src/main/java/ar/fiuba/tdd/tp/api/Request.java
@@ -17,4 +17,8 @@ public class Request implements Serializable {
     public void setSomething(String something) {
         this.something = something;
     }
+
+    public boolean isExit() {
+        return something.equals("exit game");
+    }
 }
