6a8a5e824b2c6feb4caa0b49b0740fc079b3ffb3
mtejedor <mtejedor@hexacta.com>
2016-04-28 01:26:35 -0300

    ServerOutput: added choosePort and closedPort messages. print private method for solving CPD

--- server/src/main/java/ar/fiuba/tdd/tp/api/ServerOutput.java
+++ server/src/main/java/ar/fiuba/tdd/tp/api/ServerOutput.java
@@ -6,31 +6,43 @@ package ar.fiuba.tdd.tp.api;
 public class ServerOutput {
     private static final String PREFIX = "Server> ";
 
-    public static void clientConnected(int localPort) {
-        System.out.println(PREFIX + "Client connected on port " + localPort);
+    private static void print(String string) {
+        System.out.println(PREFIX + string);
     }
 
-    public static void clientDisconnected(int localPort) {
-        System.out.println(PREFIX + "Offline client on port " + localPort);
+    public static void clientConnected(Integer localPort) {
+        print("Client connected on port " + localPort);
+    }
+
+    public static void clientDisconnected(Integer localPort) {
+        print("Offline client on port " + localPort);
     }
 
     public static void welcomeMessage() {
-        System.out.println(PREFIX + "Welcome to Majora's Mask game service, open a new game typing 'load game'");
+        print("Welcome to Majora's Mask game service, open a new game typing 'load game'");
     }
 
     public static void newGame(Integer port) {
-        System.out.println(PREFIX + "New game opened at port " + port);
+        print("New game opened at port " + port);
     }
 
-    public static void noPortsAvailable(int basePort, int endPort) {
-        System.out.println(PREFIX + "No ports available from " + basePort + " to " + endPort);
+    public static void noPortsAvailable() {
+        print("No ports available");
     }
 
     public static void unknownCommand() {
-        System.out.println(PREFIX + "Unknown command, try with 'close' 'exit' or 'connect'");
+        print("Unknown command, try with 'close' 'exit' or 'connect'");
     }
 
     public static void unopenedConnection(Integer port) {
-        System.out.println(PREFIX + "Couldn't open connection at port " + port);
+        print("Couldn't open connection at port " + port);
+    }
+
+    public static void choosePort() {
+        print("Please choose a port to close");
+    }
+
+    public static void closedPort(Integer port) {
+        print("The port " + port + " is now closed");
     }
 }
