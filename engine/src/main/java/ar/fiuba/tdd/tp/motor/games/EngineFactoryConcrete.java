3e8d27c9295b7919004e45fc844b7a915f753a53
German Guzelj <german.guzelj@almundo.com>
2016-04-27 22:58:24 -0300

    //TODO Engine factory

--- engine/src/main/java/ar/fiuba/tdd/tp/motor/games/EngineFactoryConcrete.java
+++ engine/src/main/java/ar/fiuba/tdd/tp/motor/games/EngineFactoryConcrete.java
@@ -1,12 +1,24 @@
 package ar.fiuba.tdd.tp.motor.games;
 
 
+import ar.fiuba.tdd.tp.motor.game.games.zorktype.EngineZork;
+import ar.fiuba.tdd.tp.motor.game.games.zorktype.fetch.GameFetch;
 import ar.fiuba.tdd.tp.motor.games.hanoi.EngineHanoi;
 
-public class EngineFactoryConcrete implements EngineFactory {
-    private static EngineFactoryConcrete uniqueInstance = null;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Supplier;
 
-    private EngineFactoryConcrete() {
+public class EngineFactoryConcrete implements EngineFactory {
+
+    private static EngineFactoryConcrete uniqueInstance = null;
+    private final HashMap<String, Supplier<Engine>> engineCreator;
+
+    public EngineFactoryConcrete() {
+        this.engineCreator = new HashMap<>();
+        this.engineCreator.put("hanoi", this::createEngineHanoi);
+        //TODO Etc...
 
     }
 
@@ -15,6 +27,18 @@ public class EngineFactoryConcrete implements EngineFactory {
             uniqueInstance = new EngineFactoryConcrete();
         }
         return uniqueInstance;
+    }
+
+    public Optional<Engine> createGame(String name) {
+        Supplier<Engine> supplier = engineCreator.get(name.toLowerCase());
+        if (Objects.nonNull(supplier)) {
+            return Optional.of(supplier.get());
+        }
+        return Optional.empty();
+    }
+
+    public Engine createGameFetch() {
+        return new EngineZork(new GameFetch());
     }
 
     public Engine createEngineHanoi() {
