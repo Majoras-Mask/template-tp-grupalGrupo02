965b73dd9df3a600a3790daa7ffe27b0d1a77478
mtejedor <mtejedor@hexacta.com>
2016-05-18 23:11:15 -0300

    fixed and refactor Cursed Object

--- engine/src/main/java/ar/fiuba/tdd/tp/games/CursedObjectBuilder.java
+++ engine/src/main/java/ar/fiuba/tdd/tp/games/CursedObjectBuilder.java
@@ -17,105 +17,109 @@ public class CursedObjectBuilder implements GameBuilder {
         Content room2 = new Content("room2");
         Content room3 = new Content("room3");
         Content player = new Content("player");
-        Content cursedObject = new Content("cursed object");
+        Content cursedObject = new Content("cursedObject");
         Content door1 = new Content("door");
         Content door2 = new Content("door");
         Content thief = new Content("thief");
-        cursedObject.addCommand("pick", (params) -> true, (params) -> {
-            player.put(player.getContainer().take("cursed object"));
-            return "You picked a cursed object";
-        });
-        door1.addCommand("open", (params) -> player.has("cursed object"), (params) -> {
-            room2.put(room1.take("player"));
-            return "You opened a door and walked to room2";
-        });
-        door2.addCommand("open", (params) -> !player.has("cursed object"), (params) -> {
-            room3.put(room2.take("player"));
-            return "You opened a door and walked to room3";
-        });
-        thief.addCommand("hello", (params) -> true, (params) -> {
-            player.take("cursed object");
-            room2.take("thief");
-            return "The thief stoled your cursed object and ran away";
-        });
         room1.put(player);
         room1.put(cursedObject);
         room1.put(door1);
         room2.put(door2);
         room2.put(thief);
-        WinCondition playerIsInRoom3 = () -> room3.has("player");
-        GameCommand pick = makePick(player);
-        GameCommand lookAround = makeLookAround(player);
-        GameCommand open = makeOpen(player);
-        GameCommand hello = makeHello(player);
-        game.setWinCondition(playerIsInRoom3);
-        game.setCommand(pick);
-        game.setCommand(lookAround);
-        game.setCommand(open);
-        game.setCommand(hello);
+        addContentCommands(player, door1, door2, room1, room2, room3, thief, cursedObject);
+        game.setWinCondition(() -> room3.has("player"));
+        game.setCommand(makePick(player));
+        game.setCommand(makeLookAround(player));
+        game.setCommand(makeOpen(player));
+        game.setCommand(makeHello(player));
         return game;
     }
 
+    private void addContentCommands(Content player, Content door1, Content door2, Content room1, Content room2, Content room3, Content thief, Content cursedObject) {
+        cursedObject.addCommand("pick", (params) -> true, (params) -> {
+            player.put(player.getContainer().take("cursedObject"));
+            return "You picked a cursedObject";
+        });
+        door1.addCommand("open", (params) -> player.has("cursedObject"), (params) -> {
+            room2.put(room1.take("player"));
+            return "You opened a door and walked to room2";
+        });
+        door2.addCommand("open", (params) -> !player.has("cursedObject"), (params) -> {
+            room3.put(room2.take("player"));
+            return "You opened a door and walked to room3";
+        });
+        thief.addCommand("hello", (params) -> true, (params) -> {
+            player.take("cursedObject");
+            room2.take("thief");
+            return "The thief stoled your cursedObject and ran away";
+        });
+    }
+
     private GameCommand makeOpen(Content player) {
-        CommandValidator openParser = (command) -> {
+        return new GameCommand((command) -> {
             Pattern openPattern = Pattern.compile("open .*");
             Matcher openMatcher = openPattern.matcher(command);
             return openMatcher.find();
-        };
-        CommandExecutor openExecutor = (params) -> {
+        }, (params) -> {
             Content playerRoom = player.getContainer();
             if (playerRoom.has(params[0])) {
                 return playerRoom.get(params[0]).doCommand("open");
             } else {
                 return "Can't do open on " + params[0];
             }
-        };
-
-        return new GameCommand(openParser, openExecutor, (command) -> new String[0]);
+        }, (command) -> {
+            String[] split = command.split(" ");
+            String[] params = new String[1];
+            params[0] = split[1];
+            return params;
+        });
     }
 
     private GameCommand makeHello(Content player) {
-        CommandValidator helloParser = (command) -> {
+        return new GameCommand((command) -> {
             Pattern helloPattern = Pattern.compile("say hello .*");
             Matcher helloMatcher = helloPattern.matcher(command);
             return helloMatcher.find();
-        };
-        CommandExecutor helloExecutor = (params) -> {
+        }, (params) -> {
             Content playerRoom = player.getContainer();
             if (playerRoom.has(params[0])) {
                 return playerRoom.get(params[0]).doCommand("hello");
             } else {
                 return "Can't do hello on " + params[0];
             }
-        };
-
-        return new GameCommand(helloParser, helloExecutor, (command) -> new String[0]);
+        }, (command) -> {
+            String[] split = command.split(" ");
+            String[] params = new String[1];
+            params[0] = split[2];
+            return params;
+        });
     }
 
     private GameCommand makePick(Content player) {
-        CommandValidator pickParser = (command) -> {
+        return new GameCommand((command) -> {
             Pattern pickPattern = Pattern.compile("pick .*");
             Matcher pickMatcher = pickPattern.matcher(command);
             return pickMatcher.find();
-        };
-        CommandExecutor pickExecutor = (params) -> {
+        }, (params) -> {
             Content playerRoom = player.getContainer();
             if (playerRoom.has(params[0])) {
                 return playerRoom.get(params[0]).doCommand("pick");
             } else {
                 return "Can't do pick on " + params[0];
             }
-        };
-        return new GameCommand(pickParser, pickExecutor, (command) -> new String[0]);
+        }, (command) -> {
+            String[] split = command.split(" ");
+            String[] params = new String[1];
+            params[0] = split[1];
+            return params;
+        });
     }
 
     private GameCommand makeLookAround(Content player) {
-        CommandValidator lookAroundParser = (command) -> "look around".equals(command);
-        CommandExecutor lookAroundExecutor = (empty) -> {
+        return new GameCommand((command) -> "look around".equals(command), (empty) -> {
             Content playerRoom = player.getContainer();
             return playerRoom.getName() + " has " + playerRoom.getContentsList();
-        };
-        return new GameCommand(lookAroundParser, lookAroundExecutor, (command) -> new String[0]);
+        }, (command) -> new String[0]);
     }
 
 }
--- engine/src/test/java/ar/fiuba/tdd/tp/games/CursedObjectTest.java
+++ engine/src/test/java/ar/fiuba/tdd/tp/games/CursedObjectTest.java
@@ -17,7 +17,7 @@ public class CursedObjectTest {
 
     @Test
     public void winGame() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         game.command("say hello thief");
         assertEquals("You won the game!", game.command("open door"));
@@ -41,18 +41,18 @@ public class CursedObjectTest {
     //Tests in first room
     @Test
     public void lookAroundFirstRoom() {
-        assertEquals("room1 has door-cursed object-player", game.command("look around"));
+        assertEquals("room1 has door-cursedObject-player", game.command("look around"));
     }
 
     @Test
     public void lookAroundFirstRoomAfterPickedCursedObject() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         assertEquals("room1 has door-player", game.command("look around"));
     }
 
     @Test
     public void pickCursedObject(){
-        assertEquals("You picked a cursed object",game.command("pick cursed object"));
+        assertEquals("You picked a cursedObject",game.command("pick cursedObject"));
     }
 
     @Test
@@ -62,7 +62,7 @@ public class CursedObjectTest {
 
     @Test
     public void openCursedObject(){
-        assertEquals("Can't do open on cursed object", game.command("open cursed object"));
+        assertEquals("Can't do open on cursedObject", game.command("open cursedObject"));
     }
 
     @Test
@@ -72,7 +72,7 @@ public class CursedObjectTest {
 
     @Test
     public void openDoorAfterPickedCursedObject(){
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         assertEquals("You opened a door and walked to room2",game.command("open door"));
     }
 
@@ -83,18 +83,18 @@ public class CursedObjectTest {
 
     @Test
     public void sayHelloCursedObject(){
-        assertEquals("Can't do hello on cursed object",  game.command("say hello cursed object"));
+        assertEquals("Can't do hello on cursedObject",  game.command("say hello cursedObject"));
     }
     //Tests in second room
     @Test
     public void lookAroundSecondRoom() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         assertEquals("room2 has door-thief-player", game.command("look around"));
     }
     @Test
     public void lookAroundSecondRoomAfterTalkWithThief() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         game.command("say hello thief");
         assertEquals("room2 has door-player", game.command("look around"));
@@ -102,21 +102,21 @@ public class CursedObjectTest {
 
     @Test
     public void pickThief() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         assertEquals("Can't do pick on thief",  game.command("pick thief"));
     }
 
     @Test
     public void pickDoor() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         assertEquals("Can't do pick on door",  game.command("pick door"));
     }
 
     @Test
     public void pickDoorAfterWithoutCursedObject() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         game.command("say hello thief");
         assertEquals("Can't do pick on door",  game.command("pick door"));
@@ -124,33 +124,30 @@ public class CursedObjectTest {
 
     @Test
     public void openThief() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         assertEquals("Can't do open on thief",  game.command("open thief"));
     }
 
     @Test
     public void openDoor2WithCursedObject() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         assertEquals("Can't do open on door",  game.command("open door"));
     }
 
     @Test
     public void sayHelloThief() {
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
-        assertEquals("The thief stoled your cursed object and ran away",  game.command("say hello thief"));
+        assertEquals("The thief stoled your cursedObject and ran away",  game.command("say hello thief"));
     }
 
     @Test
     public void sayHelloDoor2(){
-        game.command("pick cursed object");
+        game.command("pick cursedObject");
         game.command("open door");
         assertEquals("Can't do hello on door",  game.command("say hello door"));
     }
-
-
-
 
 }
