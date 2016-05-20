a5136f1eea4136d4133a452763c08c0b1d15511a
Fernando Cancellara <fdcancellara@gmail.com>
2016-05-20 13:05:36 -0300

    moving things around.

--- engine/src/main/java/ar/fiuba/tdd/tp/engine/behavior/LookAround.java
+++ engine/src/main/java/ar/fiuba/tdd/tp/engine/behavior/LookAround.java
@@ -5,9 +5,7 @@ import ar.fiuba.tdd.tp.engine.Game;
 public class LookAround implements Behavior {
     Game game;
     private String hasMessage = HAS_MESSAGE;
-    private String messageA = A_MESSAGE;
     private static final String HAS_MESSAGE = "has";
-    private static final String A_MESSAGE = "a";
 
     public LookAround(Game game) {
         this.game = game;
@@ -17,7 +15,7 @@ public class LookAround implements Behavior {
         StringBuffer message = new StringBuffer();
         message.append(game.getPlayer().currentRoomName() + " " + hasMessage + ":");
         for (String component : game.getPlayer().listOfWhatISee()) {
-            message.append(" " + messageA + " " + component + ".");
+            message.append(" " + component + ".");
         }
 
         return message.toString();
