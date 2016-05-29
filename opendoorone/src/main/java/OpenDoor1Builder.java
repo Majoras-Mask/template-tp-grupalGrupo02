9a19a2757fe21e7ab79e0659a075de34b235d453
Fernando Cancellara <fdcancellara@gmail.com>
2016-05-28 21:12:00 -0300

    starting to make the game open door 1 multiplayerable.

--- opendoorone/src/main/java/OpenDoor1Builder.java
+++ opendoorone/src/main/java/OpenDoor1Builder.java
@@ -5,32 +5,37 @@ import ar.fiuba.tdd.tp.engine.utils.CommandsUtils;
 
 @SuppressWarnings("CPD-START")
 public class OpenDoor1Builder implements GameBuilder {
+    private static final int MAXPLAYERS = 2;
 
     @Override
     public Game build() {
-        Game game = new Game();
-        Content room1 = new Content("room1");
-        Content room2 = new Content("room2");
-        Content player = new Content("player");
-        Content key = new Content("key");
-        Content door = new Content("door");
-        room1.put(player);
-        room1.put(key);
-        room1.put(door);
-        addContentCommands(player, key, door, room1, room2);
-        game.setWinCondition(() -> room2.has("player"));
-        game.setLoseCondition(() -> false);
-        game.setCommand(CommandsUtils.getSameRoomCommand("pick .*", "pick", player, 1));
-        game.setCommand(CommandsUtils.getLookAroundCommand("look around", player));
-        game.setCommand(CommandsUtils.getSameRoomCommand("open .*", "open", player, 1));
+        Game game = new Game(MAXPLAYERS) {
+            @Override
+            public void addPlayer(int playerID) {
+                Content room1 = new Content("room1");
+                Content room2 = new Content("room2");
+                Content player = new Content("player" + playerID);
+                Content key = new Content("key");
+                Content door = new Content("door");
+                room1.put(player);
+                room1.put(key);
+                room1.put(door);
+                addContentCommands(player, key, door, room1, room2);
+                this.addWinCondition(playerID, () -> room2.has("player" + playerID));
+                this.addLoseCondition(playerID, () -> false);
+                this.setCommand(playerID, CommandsUtils.getSameRoomCommand("pick .*", "pick", player, 1));
+                this.setCommand(playerID, CommandsUtils.getLookAroundCommand("look around", player));
+                this.setCommand(playerID, CommandsUtils.getSameRoomCommand("open .*", "open", player, 1));
+            }
+        };
+
         return game;
     }
 
-
     private void addContentCommands(Content player, Content key, Content door, Content room1, Content room2) {
-        CommandsUtils.addPickCommand(player, key, "key", "pick");
-        door.addCommand("open", (params) -> player.has("key"), (params) -> {
-            room2.put(room1.take("player"));
+        CommandsUtils.addPickCommand(player, key, key.getName(), "pick");
+        door.addCommand("open", (params) -> player.has(key.getName()), (params) -> {
+            room2.put(room1.take(player.getName()));
             return "You opened a door and walked to room2";
         });
     }
--- opendoorone/src/test/java/OpenDoor1Test.java
+++ opendoorone/src/test/java/OpenDoor1Test.java
@@ -8,80 +8,83 @@ import static org.junit.Assert.assertEquals;
 public class OpenDoor1Test {
     private final OpenDoor1Builder builder = new OpenDoor1Builder();
     private Game game;
+    private static final int PLAYERID = 1;
+    private static final String WON_MESSAGE = "Won";
 
     @Before
     public void init() {
         game = builder.build();
+        game.joinPlayer(PLAYERID);
     }
 
     @Test
     public void winGame() {
-        game.command("pick key");
-        assertEquals("You won the game!", game.command("open door"));
+        game.command(PLAYERID, "pick key");
+        assertEquals(WON_MESSAGE, game.command(PLAYERID, "open door"));
     }
 
     @Test
     public void alreadyWonGameOpenDoor() {
-        game.command("pick key");
-        game.command("open door");
-        assertEquals("You won the game!", game.command("open door"));
+        game.command(PLAYERID, "pick key");
+        game.command(PLAYERID, "open door");
+        assertEquals(WON_MESSAGE, game.command(PLAYERID, "open door"));
     }
 
     @Test
     public void alreadyWonGamePickKey() {
-        game.command("pick key");
-        game.command("open door");
-        assertEquals("You won the game!", game.command("pick key"));
+        game.command(PLAYERID, "pick key");
+        game.command(PLAYERID, "open door");
+        assertEquals(WON_MESSAGE, game.command(PLAYERID, "pick key"));
     }
 
     @Test
     public void alreadyWonGameLookAround() {
-        game.command("pick key");
-        game.command("open door");
-        assertEquals("You won the game!", game.command("look around"));
+        game.command(PLAYERID, "pick key");
+        game.command(PLAYERID, "open door");
+        assertEquals(WON_MESSAGE, game.command(PLAYERID, "look around"));
     }
 
     @Test
     public void alreadyWonGameInvalidCommand() {
-        game.command("pick key");
-        game.command("open door");
-        assertEquals("You won the game!", game.command("something"));
+        game.command(PLAYERID, "pick key");
+        game.command(PLAYERID, "open door");
+        assertEquals(WON_MESSAGE, game.command(PLAYERID, "something"));
     }
 
     @Test
     public void invalidPick() {
-        assertEquals("Can't do pick on something", game.command("pick something"));
+        assertEquals("Can't do pick on something", game.command(PLAYERID, "pick something"));
     }
 
     @Test
     public void invalidCommand() {
-        assertEquals("invalid command", game.command("something"));
+        assertEquals("invalid command", game.command(PLAYERID, "something"));
     }
 
     @Test
     public void cantOpenDoorWithoutKey() {
-        assertEquals("Can't do open on door", game.command("open door"));
+        assertEquals("Can't do open on door", game.command(PLAYERID, "open door"));
     }
 
     @Test
     public void lookAround() {
-        assertEquals("room1 has door-key-player", game.command("look around"));
+        assertEquals("room1 has door-player1-key", game.command(PLAYERID, "look around"));
     }
 
     @Test
     public void lookAroundAfterPickAKey() {
-        game.command("pick key");
-        assertEquals("room1 has door-player", game.command("look around"));
+        game.command(PLAYERID, "pick key");
+        assertEquals("room1 has door-player" + PLAYERID, game.command(PLAYERID, "look around"));
     }
 
     @Test
     public void openKey() {
-        assertEquals("Can't do open on key", game.command("open key"));
+        assertEquals("Can't do open on key", game.command(PLAYERID, "open key"));
     }
 
     @Test
     public void pickDoor() {
-        assertEquals("Can't do pick on door", game.command("pick door"));
+        assertEquals("Can't do pick on door", game.command(PLAYERID, "pick door"));
     }
 
 }
