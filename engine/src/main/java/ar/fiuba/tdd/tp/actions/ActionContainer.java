2553fc3bd8ee3beb2c9c2fcee713d1840b64be0b
Kevin <addinkevin@gmail.com>
2016-05-29 22:02:00 -0300

    Action container

---
+++ engine/src/main/java/ar/fiuba/tdd/tp/actions/ActionContainer.java
@@ -0,0 +1,27 @@
+package ar.fiuba.tdd.tp.actions;
+
+import ar.fiuba.tdd.tp.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kevin on 29/05/16.
+ */
+public class ActionContainer implements Action {
+
+    List<Action> actions = new ArrayList<>();
+
+    public void addAction(Action action) {
+        if (!actions.contains(action)) {
+            actions.add(action);
+        }
+    }
+
+    @Override
+    public void execute(Context context) {
+        for (Action action:actions) {
+            action.execute(context);
+        }
+    }
+}
