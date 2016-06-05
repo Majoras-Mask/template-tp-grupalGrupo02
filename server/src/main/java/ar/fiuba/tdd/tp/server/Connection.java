67e702e2927cc4fa2a1b808ec4c126d9ad81cfa4
Kevin <addinkevin@gmail.com>
2016-06-05 01:56:48 -0300

    Logica en Connection para que mande mensaje a los players.

--- server/src/main/java/ar/fiuba/tdd/tp/server/Connection.java
+++ server/src/main/java/ar/fiuba/tdd/tp/server/Connection.java
@@ -1,27 +1,33 @@
 package ar.fiuba.tdd.tp.server;
 
 import ar.fiuba.tdd.tp.Game;
+import ar.fiuba.tdd.tp.Sender;
+import ar.fiuba.tdd.tp.server.communication.Response;
 import ar.fiuba.tdd.tp.server.io.ServerOutput;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Exchanger;
 
 import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
-public class Connection extends Thread {
+public class Connection extends Thread implements Sender {
     private final ServerSocket serverSocket;
     private final Game game;
     private HashMap<String, Socket> clientSockets = new HashMap<>();
+    private HashMap<String, ObjectOutputStream> clientOutputs = new HashMap<>();
 
     public Connection(ServerSocket serverSocket, Game game) {
         this.serverSocket = requireNonNull(serverSocket);
         this.game = game;
+        this.game.setSender(this);
     }
 
     public void closeConnection() {
@@ -46,6 +52,7 @@ public class Connection extends Thread {
                 String playerID = game.getPlayerIDAvailable();
                 ClientConnection client = new ClientConnection(clientSocket, game, serverSocket,playerID);
                 clientSockets.put(playerID, clientSocket);
+                clientOutputs.put(playerID, new ObjectOutputStream(clientSocket.getOutputStream()));
                 client.start();
             }
         } catch (IOException e) {
@@ -53,4 +60,24 @@ public class Connection extends Thread {
         }
     }
 
+    @Override
+    public void sendAll(String message) {
+        for (String playerID : clientSockets.keySet()) {
+            send(playerID, message);
+        }
+    }
+
+    @Override
+    public void send(String playerID, String message) {
+        try {
+            if (clientSockets.containsKey(playerID) && !clientSockets.get(playerID).isClosed()) {
+                ObjectOutputStream outputStream = clientOutputs.get(playerID);
+                Response response = new Response(message);
+                outputStream.writeObject(response);
+                outputStream.flush();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 }
