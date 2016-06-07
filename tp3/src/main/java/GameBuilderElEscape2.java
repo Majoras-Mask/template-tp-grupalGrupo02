45b8cea2a0f40fc8b96fb83fe4f76491ba06a72d
Kevin <addinkevin@gmail.com>
2016-06-07 17:33:25 -0300

    Algunos fixes a game.

---
+++ tp3/src/main/java/GameBuilderElEscape2.java
@@ -0,0 +1,26 @@
+/*
+import ar.fiuba.tdd.tp.*;
+import ar.fiuba.tdd.tp.commands.Command;
+import ar.fiuba.tdd.tp.conditions.Condition;
+
+
+@SuppressWarnings("CPD-START")
+public class GameBuilderElEscape2 implements GameBuilder {
+
+    private static final String PLAYER1 = "player1";
+    private static final String PLAYER2 = "player2";
+    private static final String PLAYER3 = "player3";
+    private static final String PLAYER4 = "player4";
+
+    public Game build() {
+        GameConcrete gameConcrete = new GameConcrete();
+        Builder builder = new Builder(gameConcrete);
+
+
+
+        return gameConcrete;
+    }
+
+
+}
+*/
---
+++ tp3/src/main/java/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+
+}
+
+/*
+import ar.fiuba.tdd.tp.Game;
+import ar.fiuba.tdd.tp.Sender;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main implements Sender {
+
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Game game = new GameBuilderHanoi().build();
+        game.setSender(this);
+        game.startLoop();
+        String playerID = game.getPlayerIDAvailable();
+        while (true) {
+            try {
+                System.out.print(">");
+                String str = br.readLine();
+                String response = game.executeCommand(playerID, str);
+                if (response != null) {
+                    System.out.println(">" + response);
+                }
+            } catch ( Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void sendAll(String message) {
+        System.out.print("SendAll: ");
+        System.out.println(message);
+    }
+
+    public void send(String playerID, String message) {
+        System.out.print("Send To " + playerID +": ");
+        System.out.println(message);
+    }
+}
+*/
