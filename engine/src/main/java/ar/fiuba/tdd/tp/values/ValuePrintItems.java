896e2ec7b6c0962319e09a74ba126f1e9bf4fbac
Kevin <addinkevin@gmail.com>
2016-06-15 16:04:18 -0300

    Agregado de ValuePrintItems. Imprime los objetos contenidos en otro objeto.

---
+++ engine/src/main/java/ar/fiuba/tdd/tp/values/ValuePrintItems.java
@@ -0,0 +1,37 @@
+package ar.fiuba.tdd.tp.values;
+
+import ar.fiuba.tdd.tp.Context;
+import ar.fiuba.tdd.tp.ObjectInterface;
+
+import java.util.List;
+
+/**
+ * Created by kevin on 15/06/16.
+ */
+public class ValuePrintItems implements Value {
+
+    private Value objectDescription;
+
+    public ValuePrintItems(Value objectDescription) {
+        this.objectDescription = objectDescription;
+    }
+
+    private String getAllItemsMessage(ObjectInterface objectInterface) {
+        List<ObjectInterface> objects = objectInterface.getObjects();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0 ; i < objects.size() - 1; i++) {
+            stringBuilder.append(objects.get(i).getDescription());
+            stringBuilder.append(" , ");
+        }
+        stringBuilder.append(objects.get(objectInterface.getSize() - 1).getDescription());
+        stringBuilder.append(".");
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String getValue(Context context) {
+        ObjectInterface object = context.getObject(objectDescription.getValue(context));
+        return getAllItemsMessage(object);
+    }
+}
